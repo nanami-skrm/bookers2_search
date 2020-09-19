aa4001ba4804042307cd4aa4c29cc374268a958a
@nanami-skrm <skrm773@gmail.com>
2020-09-19 13:37:57 +0900

    [Finish]bookers2-debug

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,9 +24,10 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user != current_user
+      redirect_to books_path
+    end
   end
-
-
 
   def update
     @book = Book.find(params[:id])
